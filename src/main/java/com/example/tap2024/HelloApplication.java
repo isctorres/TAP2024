a8ab0137641e9138f7873aa4adb804526173a794
Ruben Torres Frias <isctorres@gmail.com>
2024-02-08 18:46:42 -0600

    calculadora

--- src/main/java/com/example/tap2024/HelloApplication.java
+++ src/main/java/com/example/tap2024/HelloApplication.java
@@ -1,8 +1,10 @@
 package com.example.tap2024;
 
+import com.example.tap2024.vistas.Calculadora;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,11 +12,14 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        //FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        //Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(new BorderPane());
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
+
+        new Calculadora();
     }
 
     public static void main(String[] args) {
